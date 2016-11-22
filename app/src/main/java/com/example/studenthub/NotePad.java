9e5e6c17ed33ad186577f815855e50b06ceecfc0
Matt Metcalfe <mattmetcalfe@utexas.edu>
2016-11-22 17:29:45 -0600

    misc test

--- app/src/main/java/com/example/studenthub/NotePad.java
+++ app/src/main/java/com/example/studenthub/NotePad.java
@@ -2,6 +2,7 @@ package com.example.studenthub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class NotePad extends AppCompatActivity {
     //ADDED comment for pushing
@@ -9,5 +10,7 @@ public class NotePad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
