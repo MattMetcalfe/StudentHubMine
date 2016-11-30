b712e86c61aee08120f612cd6294897ab4dfc50e
bmwatson214 <bailey.watson214@utexas.edu>
2016-11-29 19:47:36 -0600

    Task DB complete & todo.xml updated

--- app/src/main/java/com/example/studenthub/Task.java
+++ app/src/main/java/com/example/studenthub/Task.java
@@ -1,6 +1,7 @@
 package com.example.studenthub;
 
 import java.sql.Time;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -10,26 +11,32 @@ public class Task{
     //task private fields
     private String id;
     private String label;
+    private String timeStamp;
     //task constructors
     public Task(){
         id=null;
         label="";
+        timeStamp= Calendar.getInstance().toString();
     }
     public Task(String lab, String rownum){
         id=rownum;
         label=lab;
+        timeStamp =Calendar.getInstance().toString();
     }
     public Task(String lab, int rownum){
         id=String.valueOf(rownum);
         label= lab;
+        timeStamp = Calendar.getInstance().toString();
     }
     //task getters
     public String getId(){return id;}
     public String getLabel() {
         return label;
     }
+    public String getTime() {return timeStamp;}
     //task setters
     public void setId(String i){this.id=i;}
     public void setId(int i){this.id=String.valueOf(i);}
     public void setLabel(String l) {this.label = l;}
+    public void setTime(String t){this.timeStamp = t;}
 }
--- app/src/main/java/com/example/studenthub/TaskDBHandler.java
+++ app/src/main/java/com/example/studenthub/TaskDBHandler.java
@@ -2,8 +2,12 @@ package com.example.studenthub;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Bailey on 11/29/2016.
@@ -17,6 +21,7 @@ public class TaskDBHandler extends SQLiteOpenHelper{
     // Task Table Columns names
     private static final String KEY_ID = "id";
     private static final String KEY_LABEL = "label";
+    private static final String KEY_TIME = "time";
 
     public TaskDBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -26,7 +31,9 @@ public class TaskDBHandler extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db){
         String CREATE_TABLE_COMMAND= "CREATE TABLE " + TABLE_NAME +" ( "
                 +KEY_ID+" TEXT, "
-                +KEY_LABEL+" TEXT" +" )";
+                +KEY_LABEL+" TEXT, "
+                +KEY_TIME+" TEXT"
+                +" )";
         db.execSQL(CREATE_TABLE_COMMAND);
     }
 
@@ -42,16 +49,75 @@ public class TaskDBHandler extends SQLiteOpenHelper{
         ContentValues values = new ContentValues();
         values.put(KEY_ID, task.getId());
         values.put(KEY_LABEL, task.getLabel());
+        values.put(KEY_TIME, task.getTime());
         db.insert(TABLE_NAME, null, values);
         db.close();
     }
-
-    //Update Task in DB, given...
-
-    //Read Task from DB, given...
-
+    //Update Task in DB (depn. on id)
+    public void updateTask(Task task){//needs to depend on row num
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID,task.getId());
+        values.put(KEY_LABEL,task.getLabel());
+        values.put(KEY_TIME, task.getTime());
+        db.update(TABLE_NAME, values, KEY_ID+" = ?",
+                new String[] { String.valueOf(task.getId()) });
+    }
+    //Delete Task from DB, (depn. on id)
+    public void deleteTask(Task task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, KEY_ID + " = ?",
+                new String[] { String.valueOf(task.getId()) });
+        taskIndexReset();
+        db.close();
+    }
     //Read All (return list)
+    public List<Task> getAllTasks() {
+        List<Task> taskList = new ArrayList<Task>();
+        String queryCommand = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(queryCommand, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Task task = new Task();
+                task.setId(cursor.getString(0));
+                task.setLabel(cursor.getString(1));
+                task.setTime(cursor.getString(2));
+                taskList.add(task);
+            } while (cursor.moveToNext());
+        }
+        return taskList;
+    }
+    //Read Task from DB, given row number
+    public Task getTask(Long row_num) {
+        List<Task> taskList = new ArrayList<Task>();
+        Task result = new Task();
+        Integer r = row_num.intValue();
+        taskList = getAllTasks();
+        result = taskList.get(r);
+        return result;
+    }
 
-    //Delete Task from DB, given...
+    //Reset ID indices (use after deletion)
+    public void taskIndexReset(){
+        String queryCommand = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(queryCommand, null);
+        int i =0;
+        if (cursor.moveToFirst()) {
+            do {//id, label, time
+                ContentValues values = new ContentValues();
+                String oldid=cursor.getString(0);
+                values.put(KEY_ID,String.valueOf(i));
+                i++;
+                values.put(KEY_LABEL, cursor.getString(1));
+                String timestamp = cursor.getString(2);
+                values.put(KEY_TIME, timestamp);
+                db.update(TABLE_NAME, values, KEY_TIME+" = ?",
+                        new String[] { String.valueOf(timestamp) });
+            } while (cursor.moveToNext());
+        }
+    }
+
 
 }
