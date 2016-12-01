0b2c16c9f05ee1ce67b066f35c59c7f3f9e5771d
MSpearing <MSpearing@UTexas.edu>
2016-11-30 22:42:10 -0600

    No events in countdown fixed. Landing page background added.

--- app/src/main/java/com/example/studenthub/Countdown.java
+++ app/src/main/java/com/example/studenthub/Countdown.java
@@ -29,6 +29,8 @@ import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,13 +54,33 @@ public class Countdown extends AppCompatActivity {
         pageLayout.setLayoutParams(lp);
         pageLayout.setOrientation(LinearLayout.VERTICAL);
         pageLayout.setPadding(16, 16, 16, 16);
+
         TextView title = new TextView(this);
         title.setTextSize(60);
         title.setTextColor(Color.parseColor("#FFFFFF"));
         title.setText("COUNTDOWN");
         title.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
+        TextView noEvents = new TextView(this);
+        noEvents.setTextSize(30);
+        noEvents.setTextColor(Color.parseColor("#FFFFFF"));
+        noEvents.setText("\n\n\nNo Events to Track");
+        noEvents.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
+
+
+        Button openScheduler = new Button(this);
+        openScheduler.setText("Make sure your calendar is initiated");
+        openScheduler.setBackgroundColor(Color.parseColor("#CC5500"));
+        openScheduler.setTextColor(Color.parseColor("#FFFFFF"));
+        final Intent scheduler_Intent = new Intent(this, Scheduler.class);
+        openScheduler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(scheduler_Intent));
+            }
+        });
 
         Button mRefreshButton = new Button(this);
         mRefreshButton.setText("Click to Update");
@@ -111,7 +133,14 @@ public class Countdown extends AppCompatActivity {
         }
 
         pageLayout.addView(title);
-        pageLayout.addView(mRefreshButton);
+        if(mevents.size() == 0){
+            pageLayout.addView(openScheduler);
+            pageLayout.addView((noEvents));
+
+        } else {
+            pageLayout.addView(mRefreshButton);
+        }
+
         sc.addView(tableLayout);
         pageLayout.addView(sc);
         pageLayout.setBackgroundColor(Color.parseColor("#808fed"));
